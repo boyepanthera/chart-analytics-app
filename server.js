aa5f893b074f403fb56743a2b2823fd1229bf28d
root <root@localhost.localdomain>
2019-08-31 20:02:43 +0100

    push to github

--- server.js
+++ server.js
@@ -7,17 +7,23 @@ import cors from 'cors';
 import passportConfig from './config/passport';
 dotenv.config();
 const app = express (),
-    [port, url, ip] = [process.env.PORT, process.env.DB_URL,  process.env.IP];
+    [port, url, ip, mongooseOptions] = [
+        5003 || process.env.PORT, 
+        process.env.DB_URL,  
+        process.env.IP, 
+        {useNewUrlParser:true, useFindAndModify:false, autoIndex:false, useCreateIndex:true}
+    ];
     app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-mongoose.connect(url, {useNewUrlParser:true, useFindAndModify:false, autoIndex:false, useCreateIndex:true})
-    .then(()=> console.log('connected to Database'))
-    .catch((err)=> console.log(err));
-
-app.use(passport.initialize())
-passportConfig(passport);
+mongoose.connect(url, mongooseOptions)
+    .then(()=> console.log('connected to Database!!!'))
+    .catch((err)=> console.log(err))
+    .finally(()=> {
+        app.use(passport.initialize())
+        passportConfig(passport);
+    });
 
 app.get('/', (req, res)=> {
     res.json ({message:'you hit the MERN route'});
@@ -27,4 +33,4 @@ app.use('/api/v1/user/', userController);
 
 const server = app.listen(port, ip, (req, res)=> {
     console.log(`MERN api started and running in on port ${port}`);
-    });
+});
