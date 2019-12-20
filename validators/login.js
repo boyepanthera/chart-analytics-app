1e15c9fecf20f80debb8bfc687a63f9298decc41
Olanrewaju <eyiwumiolaboye@gmail.com>
2019-12-20 02:14:30 +0000

    setup basic auth

--- validators/login.js
+++ validators/login.js
@@ -5,12 +5,14 @@ const validateLogin = (data) => {
     let errors = {};
     // Convert empty fields to an empty string so we can use validator functions
     data.email = !isEmpty(data.email) ? data.email : "";
-    data.password = !isEmpty(data.password) ? data.password : "";// Email checks
+    data.password = !isEmpty(data.password) ? data.password : "";
+    // Email checks
     if (dataValidator.isEmpty(data.email)) {
         errors.email = "email field is required";
     } else if (!dataValidator.isEmail(data.email)) {
         errors.email = "input email is invalid";
-    }// Password checks
+    }
+    // Password checks
     if (dataValidator.isEmpty(data.password)) {
         errors.password = "password field is required";
     }return {
--- validators/register.js
+++ validators/register.js
@@ -2,14 +2,14 @@ import dataValidator from 'validator';
 import isEmpty from 'is-empty';
 
 function validateRegistration(data) {
-
     let errors = {};
     // Conversion of empty inputs to string since validator doesn't accept undefined
     data.firstName = !isEmpty(data.firstName) ? data.firstName : "";
     data.lastName = !isEmpty(data.lastName) ? data.lastName : "";
     data.email = !isEmpty(data.email) ? data.email : "";
     data.password = !isEmpty(data.password) ? data.password : "";
-    data.password2 = !isEmpty(data.password2) ? data.password2 : "";//    Name checks
+    data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+    //    Name checks
     if (dataValidator.isEmpty(data.firstName)) {
       errors.firstName = "First Name field is required";
     }
